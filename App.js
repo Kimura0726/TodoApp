f140c70724d14cbe2cd9fb4efaf5e3f875cb35e0
Kimura0726 <0726.kimura@gmail.com>
2018-09-09 10:28:12 +0900

    Add TextBox and Button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Platform, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform, ScrollView, FlatList, TextInput, Button, KeyboardAvoidingView } from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
@@ -7,33 +7,58 @@ export default class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      todo: [
-        { index: 1, title: "原稿を書く", done: false },
-        { index: 2, title: "犬の散歩をする", done: false }
-      ],
-      currentIndex: 2
+      todo: [], // Todoリストを空に
+      currentIndex: 0,
+      inputText: "", // テキスト入力用の箱を用意
     }
   }
 
+  // TODOリストへの追加処理
+  onAddItem = () => {
+    const title = this.state.inputText
+    if (title == "") {
+      return
+    }
+    const index = this.state.currentIndex + 1
+    const newTodo = { index: index, title: title, done: false }
+    const todo = [...this.state.todo, newTodo]
+    this.setState({
+      todo: todo,
+      currentIndex: index,
+      inputText: ""
+    })
+  }
+
   render() {
+    // ViewをKeyboardAvoidingViewへ変更、振る舞いをpaddingに
     return (
-      <View style={styles.container}>
-        {/* 2.フィルターの部分 */}
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        {/* フィルターの部分 */}
         <View style={styles.filter}>
           <Text>Filterがここに配置されます</Text>
         </View>
-        {/* 3: TODOリスト */}
+        {/* TODOリスト */}
         <ScrollView style={styles.todolist}>
           <FlatList data={this.state.todo}
             renderItem={({ item }) => <Text>{item.title}</Text>}
             keyExtractor={(item, index) => "todo_" + item.index}
           />
         </ScrollView>
-        {/* 4: 入力スペース */}
         <View style={styles.input}>
-          <Text>テキスト入力がここに配置されます</Text>
+          {/* テキスト入力とボタンを追加 */}
+          <TextInput
+            onChangeText={(text) => this.setState({ inputText: Text })}
+            value={this.state.inputText}
+            style={styles.inputText}
+          />
+          <Button
+            onPress = {this.onAddItem}
+            title = "Add"
+            color = "#841584"
+            style={styles.inputButton}
+          />
         </View>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -42,11 +67,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // 3 : paddingTopにステータスバーの高さを指定して下にずらす
     paddingTop: STATUSBAR_HEIGHT,
-    // 4 : 使わないスタイルを削除する 
-    //     alignItems: 'center',
-    //    justifyContent: 'center',
   },
   filter: {
     height: 30,
@@ -56,5 +77,12 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 30,
+    flexDirection: "row", // 下にある要素を横に並べる 
   },
+  inputText: {
+    flex: 1,
+  },
+  inputButton: {
+    width: 100,
+  }
 });
